cb95416e8e20d00818803dd5050c073707f1b655
JohnF <frostsnotes@gmail.com>
2023-04-17 10:40:55 -0700

    Add subgraph for Doodles collection (Ethereum mainnet)

--- src/token.ts
+++ src/token.ts
@@ -1,75 +1,51 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { ipfs, json, log } from '@graphprotocol/graph-ts'
 import {
-  Token,
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
-  Transfer
-} from "../generated/Token/Token"
-import { ExampleEntity } from "../generated/schema"
+  Transfer as TransferEvent,
+  Token as TokenContract
+} from '../generated/Token/Token'
+import {
+Token, User
+} from '../generated/schema'
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
+const ipfshash = "QmPMc4tcBsMqLRuCQtPmPe84bpSjrC3Ky7t3JWuHXYB4aS"
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
+export function handleTransfer(event: TransferEvent): void {
+  /* load the token from the existing Graph Node */
+  let token = Token.load(event.params.tokenId.toString())
+  if (!token) {
+    /* if the token does not yet exist, create it */
+    token = new Token(event.params.tokenId.toString())
+    token.tokenID = event.params.tokenId
+ 
+    token.tokenURI = "/" + event.params.tokenId.toString()
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    /* combine the ipfs hash and the token ID to fetch the token metadata from IPFS */
+    let metadata = ipfs.cat(ipfshash + token.tokenURI)
+    // log.info('Will there be metadata? {}', [""])
+    if (metadata) {
+      // log.info('Metadata: {}', [metadata.toString()])
+      const jsonMetadata = json.fromBytes(metadata).toObject()
+      if (jsonMetadata) {
+        /* using the metatadata from IPFS, update the token object with the values  */
+        const name = jsonMetadata.get('name')
+
+        if (name) {
+          token.name = name.toString()
+        }
+
+      }
+    }
   }
+  token.updatedAtTimestamp = event.block.timestamp
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.MAX_PUBLIC_MINT(...)
-  // - contract.MAX_SUPPLY(...)
-  // - contract.PRICE_PER_TOKEN(...)
-  // - contract.PROVENANCE(...)
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.isAllowListActive(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.numAvailableToMint(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.saleIsActive(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
+  /* set or update the owner field and save the token to the Graph Node */
+  token.owner = event.params.to.toHexString()
+  token.save()
+  
+  /* if the user does not yet exist, create them */
+  let user = User.load(event.params.to.toHexString())
+  if (!user) {
+    user = new User(event.params.to.toHexString())
+    user.save()
+  }
 }
-
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleTransfer(event: Transfer): void {}
